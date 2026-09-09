b5fbfff42cec943dd85494db9ae86ccd25863068
hwanyseo <hwanyseo@cubrid.com>
2026-09-09 15:41:29 +0900

    - warning source

--- src/cm_common/cm_utils.c
+++ src/cm_common/cm_utils.c
@@ -81,8 +81,8 @@ kill (int pid, int signo)
 
 #if defined(WINDOWS)
 int
-run_child (const char *const argv[], int wait_flag, const char *stdin_file, char *stdout_file, char *stderr_file,
-	   int *exit_status)
+run_child (const char *const argv[], int wait_flag, const char *stdin_file, const char *stdout_file,
+	   const char *stderr_file, int *exit_status)
 {
   int new_pid;
   STARTUPINFO start_info;
@@ -186,8 +186,8 @@ run_child (const char *const argv[], int wait_flag, const char *stdin_file, char
 }
 #else
 int
-run_child (const char *const argv[], int wait_flag, const char *stdin_file, char *stdout_file, char *stderr_file,
-	   int *exit_status)
+run_child (const char *const argv[], int wait_flag, const char *stdin_file, const char *stdout_file,
+	   const char *stderr_file, int *exit_status)
 {
   int pid;
 
@@ -755,7 +755,7 @@ make_temp_filename (char *tempfile, const char *prefix, int size)
 }
 
 int
-make_temp_filepath (char *tempfile, char *tempdir, char *prefix, int task_code, int size)
+make_temp_filepath (char *tempfile, const char *tempdir, const char *prefix, int task_code, int size)
 {
   struct timeval current_time;
 
